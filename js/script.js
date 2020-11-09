723d443f46e880bf125f4141a3d31d1fc236cd54
Mirko Guerra <mirkoguerra93@gmail.com>
2020-11-09 14:48:28 +0100

    Punto 1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-// mettendo tutto qui dentro, sono certo che verrà fatto tutto quando il DOM è già stato caricato
+// Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
+
+// mettendo tutto qui dentro, sono certo che il codice verrà eseguito quando il DOM è già stato caricato
 $(document).ready(function(){
+
+  // creo l'oggetto studente con le chiavi richieste
+  var studente = {
+    "nome": "Mirko",
+    "cognome": "Guerra",
+    "età": 27
+  };
+
+  // grazie al for in, attraverso l'oggetto e stampo tutte le chiavi e i rispettivi valori
+  for (var key in studente) {
+    console.log(key + " studente: " + studente[key]);
+  }
 
 });
